a6863589f8a015301d9cc5a793d442977e7e41a2
DevER-M <sankam.mithunganeshviiv@gmail.com>
2024-08-03 19:39:10 +0530

    .

---
+++ test.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import ttk
+
+window = tk.Tk()
+
+window.title('idk')
+
+label1 = ttk.Label(window,text="hmm",font="Calibri 20")
+label1.pack()
+
+window.mainloop()
