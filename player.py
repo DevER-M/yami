c36bb7820ab432a97b1571227f1073513a16b2c3
DevER-M <sankam.mithunganeshviiv@gmail.com>
2024-08-15 19:21:23 +0530

    idk

--- player.py
+++ player.py
@@ -24,53 +24,58 @@ class MusicPlayer:
         pygame.mixer.init()
         self.music = pygame.mixer.music
 
-        open_folder = ttk.Button(
+        self.open_folder = ttk.Button(
             self.root, command=self.choose_folder, text="Open", width=10
         )
-        open_folder.grid(row=0, column=0, sticky="nw")
+        self.open_folder.grid(row=0, column=0, sticky="nw")
 
-        scrollbar = ttk.Scrollbar(self.root)
-        scrollbar.grid(row=0, column=2, sticky="nes", pady=30)
+        self.scrollbar = ttk.Scrollbar(self.root)
+        self.scrollbar.grid(row=0, column=2, sticky="nes", pady=30)
 
         self.song_list = tk.Listbox(
-            self.root, borderwidth=50, yscrollcommand=scrollbar.set
+            self.root, borderwidth=50, yscrollcommand=self.scrollbar.set
         )
         self.song_list.bind("<<ListboxSelect>>", self.play)
         self.song_list.grid(row=0, column=2, sticky="nes", pady=30, padx=15)
 
-        play_button = ttk.Button(
+        self.play_button = ttk.Button(
             self.root,
             command=self.play_pause,
             text="play",
             width=10,
             image=self.pause_icon,
         )
-        play_button.grid(row=2, column=0, sticky="sw")
+        self.play_button.grid(row=2, column=0, sticky="sw")
 
     def play(self, event):
-        self.music.load(open(self.song_list.get(event.widget.curselection()[0]), "rb"))
+        for e in event.widget.curselection():
+            self.music.load(open(self.song_list.get(e), "rb"))
         self.music.play()
 
     def play_pause(self):
         if not self.is_playing:
+            self.play_button.config(image=self.pause_icon)
             self.music.unpause()
             self.is_playing = True
         else:
             self.music.pause()
+            self.play_button.config(image=self.play_icon)
             self.is_playing = False
 
     def choose_folder(self):
         folder = filedialog.askdirectory(title="Select Music Folder")
-        for file in filter(
-            lambda x: (
-                x
-                if os.path.isfile(x)
-                and x.endswith((".mp3", ".ogg", ".wav", ".m4a", ".opus"))
-                else None
-            ),
-            os.listdir(folder),
-        ):
-            self.song_list.insert("end", file)
+        self.song_list.delete(0, tk.END)
+        if folder:
+            for file in filter(
+                lambda x: (
+                    x
+                    if os.path.isfile(x)
+                    and x.endswith((".mp3", ".ogg", ".wav", ".m4a", ".opus"))
+                    else None
+                ),
+                os.listdir(folder),
+            ):
+                self.song_list.insert("end", file)
 
 
 root = ttk.Window(themename="darkly")
