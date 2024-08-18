c82af64a855eec9e6ae43565638ee0f4c9e9edc2
DevER-M <sankam.mithunganeshviiv@gmail.com>
2024-08-18 13:30:57 +0530

    nice??

--- player.py
+++ player.py
@@ -8,44 +8,45 @@ from mutagen import File
 from PIL import Image
 
 
-GEOMETRY          = "800x500"
-TITLE             = "Music Player"
-EVENT_INTERVAL    = 100
+GEOMETRY = "800x500"
+TITLE = "Music Player"
+EVENT_INTERVAL = 100
 SUPPORTED_FORMATS = (".mp3", ".ogg", ".wav", ".m4a", ".opus")
-BUTTON_WIDTH      = 10
-THEME             = "darkly"
+BUTTON_WIDTH = 10
 
 
 class PlayerState(Enum):
     PLAYING = 1
-    PAUSED  = 2
+    PAUSED = 2
     STOPPED = 3
 
+
 ctk.set_default_color_theme("theme.json")
+
+
 class MusicPlayer(ctk.CTk):
     def __init__(self):
         super().__init__()
 
-        #CONFIG
+        # CONFIG
         self.geometry(GEOMETRY)
         self.title(TITLE)
-        
 
         # STATE
-        self.playlist       = []
-        self.state          = PlayerState.STOPPED
+        self.playlist = []
+        self.state = PlayerState.STOPPED
         self.current_folder = ""
         self.playlist_index = 0
 
-        #SETUP PYGAME
+        # SETUP PYGAME
         self.initialize_pygame()
 
-        #ICONS
+        # ICONS
         self.play_icon = ctk.CTkImage(Image.open("play_arrow.png"))
         self.pause_icon = ctk.CTkImage(Image.open("pause.png"))
 
-        #FRAMES
-        self.bottom_bar = BottomBar(
+        # FRAMES
+        self.control_bar = ControlBar(
             self, 
             self.pause_icon, 
             self.play_icon, 
@@ -53,34 +54,35 @@ class MusicPlayer(ctk.CTk):
         )
         self.playlist_frame = PlaylistFrame(self)
         self.topbar = TopBar(self)
-        
-        #BINDINGS AND EVENTS
+        self.bottom_frame = BottomFrame(self)
+
+        # BINDINGS AND EVENTS
         self.bind("<F10>", self.play_next_song)
+        self.bind("<F8>",self.control_bar.play_previous)
+        self.bind("<F9>",self.control_bar.play_pause)
+        self.bind("<space>",self.control_bar.play_pause)
         self.after(100, self.check_for_events)
-        
-        #WIDGET PLACEMENT
-        self.topbar.pack(side=tk.TOP,fill=tk.X)
-        self.bottom_bar.pack(side=tk.BOTTOM,fill=tk.X)
+
+        # WIDGET PLACEMENT
+        self.topbar.pack(side=tk.TOP, fill=tk.X)
+        self.bottom_frame.pack(side=tk.BOTTOM, fill=tk.X)
+        self.control_bar.pack(side=tk.BOTTOM, fill=tk.X)
         self.playlist_frame.pack(side=tk.RIGHT)
-        
 
-    
-    
-
-    def play_next_song(self, event=None):
+    def play_next_song(self,event=None):
         if not self.playlist:
             return
-        
-        #PLAY FROM BEGINING
-        elif self.playlist_index >= len(self.playlist) -1:
+
+        # PLAY FROM BEGINING
+        elif self.playlist_index >= len(self.playlist) - 1:
             self.playlist_index = 0
         else:
-            self.playlist_index += 1  
+            self.playlist_index += 1
         self.load_and_play_song(self.playlist_index)
 
-        #UPDATE SELECTION
+        # UPDATE SELECTION
         self.playlist_frame.song_list.selection_clear(0, tk.END)
-        self.playlist_frame.song_list.select_set(self.playlist_index)         
+        self.playlist_frame.song_list.select_set(self.playlist_index)
 
     def load_and_play_song(self, index):
         self.music.unload()
@@ -88,9 +90,9 @@ class MusicPlayer(ctk.CTk):
         self.music.load(self.playlist[index])
         self.music.play()
         self.state = PlayerState.PLAYING
-        self.bottom_bar.update_play_button(self.state)
-        self.bottom_bar.start_progress_bar(self.get_song_length(self.playlist[index]))
-    
+        self.control_bar.update_play_button(self.state)
+        self.bottom_frame.start_progress_bar(self.get_song_length(self.playlist[index]))
+
     def get_song_length(self, file_path):
         audio = File(file_path)
         if audio is not None and audio.info is not None:
@@ -105,9 +107,11 @@ class MusicPlayer(ctk.CTk):
         pygame.init()
         pygame.mixer.init()
         self.music = pygame.mixer.music
-        pygame.mixer.music.set_endevent(pygame.USEREVENT)                     #CREATE USEREVENT WHEN MUSIC ENDS
 
-    #AUTOPLAY NEXT SONG AFTER SONG ENDS
+        #CREATE USEREVENT WHEN MUSIC ENDS
+        pygame.mixer.music.set_endevent(pygame.USEREVENT) 
+
+    # AUTOPLAY NEXT SONG AFTER SONG ENDS
     def check_for_events(self):
         for event in pygame.event.get():
             if event.type == pygame.USEREVENT:
@@ -115,57 +119,51 @@ class MusicPlayer(ctk.CTk):
         self.after(EVENT_INTERVAL, self.check_for_events)
 
 
-class BottomBar(ctk.CTkFrame):
+class ControlBar(ctk.CTkFrame):
     def __init__(self, parent: MusicPlayer, pause_icon, play_icon, play_next_command):
         super().__init__(parent)
-        
-        #SETUP
-        self.music_player      = parent
-        self.pause_icon        = pause_icon
-        self.play_icon         = play_icon
+
+        # SETUP
+        self.music_player = parent
+        self.pause_icon = pause_icon
+        self.play_icon = play_icon
         self.play_next_command = play_next_command
 
-        #WIDGETS
+        # WIDGETS
         self.play_button = ctk.CTkButton(
             self,
-            command = self.play_pause,
-            width   = BUTTON_WIDTH,
-            height  = 10,
-            text    = "",
-            image   = pause_icon,
-            corner_radius=10
+            command=self.play_pause,
+            width=BUTTON_WIDTH,
+            height=10,
+            text="",
+            image=pause_icon,
+            corner_radius=10,
         )
         self.next_button = ctk.CTkButton(
-            self, 
-            command = play_next_command, 
-            width   = BUTTON_WIDTH, 
-            text    = "next",
-            corner_radius=10
+            self,
+            command=play_next_command,
+            width=BUTTON_WIDTH,
+            text="next",
+            corner_radius=10,
         )
-        """self.progress_bar = ctk.CTkProgressBar(
-            self, 
-            mode="determinate", 
-            width=600
-            )"""
         self.prev_button = ctk.CTkButton(
             self,
             text="prev",
             width=BUTTON_WIDTH,
-            corner_radius=10
+            corner_radius=10,
+            command=self.play_previous,
         )
 
-        self.bottom_frame = BottomFrame(self)
-        
-        
-        #PLACEMENT
-        #self.prev_button.grid(row=1,column=0,sticky="nsew")
-        #self.play_button.grid(row=1, column=1, sticky="nsew")
-        #self.next_button.grid(row=1, column=2, sticky="nsew",)
-        #self.progress_bar.grid(row=3,column=4, sticky="senw")
-        self.bottom_frame.pack(side="bottom")
+        # PLACEMENT
+        self.prev_button.grid(row=0, column=0, sticky="nsew")
+        self.play_button.grid(row=0, column=1, sticky="nsew")
+        self.next_button.grid(
+            row=0,
+            column=2,
+            sticky="nsew",
+        )
 
-
-    def play_pause(self):
+    def play_pause(self,event=None):
         if self.music_player.state == PlayerState.PLAYING:
             self.music_player.music.pause()
             self.music_player.state = PlayerState.PAUSED
@@ -176,30 +174,64 @@ class BottomBar(ctk.CTkFrame):
 
     def update_play_button(self, state):
         if state == PlayerState.PLAYING:
-            self.play_button.config(image=self.pause_icon)
+            self.play_button.configure(image=self.pause_icon)
         else:
-            self.play_button.config(image=self.play_icon)
+            self.play_button.configure(image=self.play_icon)
+            
+
+    def play_previous(self,event=None):
+        if not self.music_player.playlist:
+            return
+
+        # PLAY FROM END
+        elif self.music_player.playlist_index == 0:
+            self.music_player.playlist_index = len(self.music_player.playlist) - 1
+        #PLAY PREVIOUS
+        else:
+            self.music_player.playlist_index -= 1
+        self.music_player.load_and_play_song(self.music_player.playlist_index)
+
+        # UPDATE SELECTION
+        self.music_player.playlist_frame.song_list.selection_clear(0, tk.END)
+        self.music_player.playlist_frame.song_list.select_set(
+            self.music_player.playlist_index
+        )
+
+
+class BottomFrame(ctk.CTkFrame):
+    def __init__(self, parent: MusicPlayer):
+        super().__init__(parent)
+        
+        #SETUP
+        self.music_player = parent
+        
+        self.progress_bar = ctk.CTkProgressBar(self)
+        self.progress_bar.pack(fill="x", expand=True)
+        
+        self.pack(fill="x")#IMP
 
     def start_progress_bar(self, song_length):
-        self.progress_bar["maximum"] = song_length
+        self.progress_bar.set(0)
+        self.song_length = song_length
         self.update_progress_bar()
-
+    
+    #PARTIALY WORKING
     def update_progress_bar(self):
         if self.music_player.state == PlayerState.PLAYING:
             song_position = self.music_player.get_song_position()
-            self.progress_bar["value"] = song_position
-            if song_position < self.progress_bar["maximum"]:
+            progress = song_position / self.song_length
+            self.progress_bar.set(progress)
+            if song_position < self.song_length:
                 self.after(EVENT_INTERVAL, self.update_progress_bar)
-
-class BottomFrame(ctk.CTkFrame):
-    def __init__(self,parent:BottomBar):
-        super().__init__(parent)
-        self.parent = parent
-        
-
-        self.progress_bar = ctk.CTkProgressBar(self, progress_color="green")
-        self.progress_bar.pack(fill="x",expand=True)
-        self.pack(fill="x")
+        elif self.music_player.state == PlayerState.PAUSED:
+            song_position = self.music_player.get_song_position()
+            progress = song_position / self.song_length
+            self.progress_bar.set(progress)
+            if song_position > self.song_length:
+                self.after(EVENT_INTERVAL,self.update_progress_bar)
+        else:
+            self.progress_bar.set(0)
+            self.after(EVENT_INTERVAL,self.update_progress_bar)
 
 
 class PlaylistFrame(ctk.CTkFrame):
@@ -228,42 +260,43 @@ class PlaylistFrame(ctk.CTkFrame):
         self.parent.load_and_play_song(index)
 
 
-
 class TopBar(ctk.CTkFrame):
-    def __init__(self,parent:MusicPlayer):
-        super().__init__(parent,fg_color="#141414")
+    def __init__(self, parent: MusicPlayer):
+        super().__init__(parent, fg_color="#141414")
+        
         self.parent = parent
-        #WIDGETS
+        
+        # WIDGETS
         self.open_folder = ctk.CTkButton(
-            self, 
-            command=self.choose_folder, 
+            self,
+            command=self.choose_folder,
             text="Open",
-            font=("roboto",15),
+            font=("roboto", 15),
             width=70,
-            
         )
         self.ytdl_placeholder = ctk.CTkButton(
-            self,
-            text="Download",
-            font=("roboto",15),
+            self, 
+            text="Download", 
+            font=("roboto", 15), 
             width=70
         )
 
-        #WIDGET PLACEMENT
-        self.open_folder.grid(row=0,column=0,sticky="w",pady=5,padx=10)
-        self.ytdl_placeholder.grid(row=0,column=1,sticky="w",pady=5,padx=10)
-        #BINDINGS
-        self.bind("<Control-o>", self.choose_folder)
-    
-    #FOR ADDING SONGS TO PLAYLIST
+        # WIDGET PLACEMENT
+        self.open_folder.grid(row=0, column=0, sticky="w", pady=5, padx=10)
+        self.ytdl_placeholder.grid(row=0, column=1, sticky="w", pady=5, padx=10)
+        
+        # BINDINGS
+        self.parent.bind("<Control-o>", self.choose_folder)
+
+    # FOR ADDING SONGS TO PLAYLIST
     def choose_folder(self, _event=None):
         self.current_folder = filedialog.askdirectory(title="Select Music Folder")
-        
-        #CLEAR PLAYLIST AND LISTBOX
+
+        # CLEAR PLAYLIST AND LISTBOX
         self.parent.playlist_frame.song_list.delete(0, tk.END)
-        self.playlist.clear()
-        
-        #FILTER MUSIC FILES
+        self.parent.playlist.clear()
+
+        # FILTER MUSIC FILES
         if self.current_folder:
             for file in filter(
                 lambda x: (
@@ -271,8 +304,8 @@ class TopBar(ctk.CTkFrame):
                 ),
                 os.listdir(self.current_folder),
             ):
-                self.playlist.append(file)
-                self.playlist_frame.song_list.insert("end", file)
+                self.parent.playlist.append(file)
+                self.parent.playlist_frame.song_list.insert("end", file)
 
 
 if __name__ == "__main__":
